439bc3f6aa0c767803609cfd4f406e0c580c17ce
Frederico Proença <joopereight@gmail.com>
2021-12-01 21:09:07 -0300

    Add project files.

---
+++ POO-Conversor-de-Moedas/CDM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Conversor_de_Moedas
+{
+    //Declaração da classe CDM (Conversor De Moeda.)
+    class CDM
+    {
+        //Declaração das variáveis double ValorReal e ValorTotal. A primeira será utilizada para converter o valor do dólar para o real, e o segundo representa o valor total.
+        double ValorReal, ValorTotal;
+        //Declaração da variável CotaDolar, utilizando automatic properties. Representa o valor que multiplicará ValorDolar.
+        double CotaDolar { get; set; }
+        //Declaração da variável ValorDolar, utilizando automatic properties. Representa o valor que será convertido em ValorReal.
+        double ValorDolar { get; set; }
+        //Declaração da variável double que carrega o valor atual do IOF, no momento da escrita do script.
+        //Valor encontrado em https://www.istoedinheiro.com.br/mudancas-no-iof-passam-a-valer-hoje-veja-como-isso-atinge-o-seu-bolso/.
+        double IOF = .0411;
+        //Declaração do construtor desta classe, que será utilizado no programa principal.
+        public CDM(double cotaDolar, double valorDolar)
+        {
+            //Associação do CotaDolar com o valor cotaDolar que será preenchido no programa principal.
+            CotaDolar = cotaDolar;
+            //Associação do ValorDolar com o valor valorDolar que será preenchido no programa principal.
+            ValorDolar = valorDolar;
+        }
+        //Método da classe que será utilizado pelo programa principal.
+        public void CalculoRealETotal()
+        {
+            //Cálculo do valor em real, inserido pelo usuário como valor em dólares e cotação.
+            ValorReal = CotaDolar * ValorDolar;
+            //Cálculo do valor total, que leva em conta o valor fixo (no script) do IOF e o valor em real calculado anteriormente.
+            ValorTotal = ValorReal + (ValorReal * IOF);
+        }
+        //Método de sobrecarga que retorna os dados calculados na classe.
+        public override string ToString()
+        {
+            //Exibição da mensagem sobre o valor a ser pago em real.
+            return "Valor a ser pago em reais = "
+                + ValorReal.ToString("C2") //Exibição do valor em real da conversão.
+                + "\nValor a ser pago de IOF = " //Exibição da mensagem sobre o valor do IOF do script.
+                + IOF.ToString("P3") //Exibição do valor IOF fixado no script.
+                + "\nValor total da compra com IOF = " //Exibição da mensagem sobre o valor total a ser pago.
+                + ValorTotal.ToString("C2"); //Exibição do valor em reais junto ao IOF.
+        }
+    }
+}
---
+++ POO-Conversor-de-Moedas/Program.cs
@@ -0,0 +1,37 @@
+﻿//Frederico Guilherme Camilli Proença RA:200925
+
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace POO_Conversor_de_Moedas
+{
+    //Programa principal com o qual o usuário interagirá.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de um novo CultureInfo CIBR, que utiliza o pt-BR como base.
+            CultureInfo CIBR = new ("pt-BR");
+            //Mudança do Culture utilizado no programa para o CIBR.
+            Thread.CurrentThread.CurrentCulture = CIBR;
+            //Declaração das variáveis double cotaDolar e valorDolar, que serão utilizadas para realizar a conversão presente em CDM.
+            double cotaDolar, valorDolar;
+            //Interação com o usuário sobre a cotação do dólar.
+            //Para achar a cota do dólar, visite https://economia.uol.com.br/cotacoes/cambio/. Valor atual em 1/12/2021, às 17:00 = 5,670.
+            Console.Write("Qual é a cotação do dólar? $");
+            //Inserção pelo usuário sobre a cotação do dólar.
+            cotaDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Interação com o usuário sobre a quantidade de dólares a serem comprados.
+            Console.Write("Quantos dólares você irá comprar? $");
+            //Inserção pelo usuário sobre a quantidade de dólares a serem comprados.
+            valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Declaração de um novo CDM conversor, que realizará os métodos presentes em CDM.
+            CDM conversor = new (cotaDolar, valorDolar);
+            //Uso do método CalculoRealETotal, declarado em CDM.
+            conversor.CalculoRealETotal();
+            //Exibição do valor pago em reais, a taxa em porcentagem do IOF, e o valor total pago em reais.
+            Console.WriteLine(conversor.ToString());
+        }
+    }
+}
